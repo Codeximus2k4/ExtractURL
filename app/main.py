f3b8eb349ce44613361a3e48c480e05cf14b9368
Nguyễn Hữu Nam Hòa <132972072+Codeximus2k4@users.noreply.github.com>
2023-09-17 00:13:57 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -30,19 +30,6 @@ def load_questions():
         questions3 = questions3 +lines
     q.close()
     questions_list.append(questions3)
-    q = open("questions-form4.txt","r",encoding="utf8")
-    questions4 =""
-    for lines in q:
-        questions4 = questions4 +lines
-    q.close()
-    questions_list.append(questions4)
-    q = open("questions-form5.txt","r",encoding="utf8")
-    questions5 =""
-    for lines in q:
-        questions5 = questions5 +lines
-    q.close()
-    questions_list.append(questions5)
-    q = open("questions-form6.txt","r",encoding="utf8")
     return questions_list
 def extract_url(url: str):
     # getting the text from url
@@ -78,7 +65,7 @@ class ExtractURL():
         completion = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             temperature = 0.5,
-            max_tokens = 30,
+            max_tokens = 50,
             messages = [
             {"role": "system", "content": f"Bạn là một bot đang hỗ trợ các lập trình viên để trích xuất thông tin từ một trang web, đây là dữ liệu raw text được crawl về : {text} "},
             {"role": "user", "content": f"\n{mylist[0]}"},  
@@ -86,13 +73,17 @@ class ExtractURL():
         )
         response = completion.choices[0].message
         
-        if response["content"].lower() == "không":
-            return None
+        answers =[]
+        answers= answers+ response["content"].split("\n")
+        if "không" in answers[0].lower():
+            continue
+        if "có" in answers[1].lower():
+            scholar_dict["status"]=1
         row =[]
         completion = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             temperature = 0.5,
-            max_tokens = 200,
+            max_tokens = 2000,
             messages = [
             {"role": "system", "content": f"Bạn là một bot đang hỗ trợ các lập trình viên để trích xuất thông tin từ một trang web, đây là dữ liệu raw text được crawl về : {text} "},
             {"role": "user", "content": f"(Chỉ đưa ra thông tin được hỏi, không giải thích gì thêm, mỗi câu trả lời viết trên một dòng) : \n{mylist[1]}"},
@@ -104,7 +95,7 @@ class ExtractURL():
         completion = openai.ChatCompletion.create(
             model = "gpt-3.5-turbo",
             temperature = 0.5,
-            max_tokens = 750,
+            max_tokens = 2000,
             messages = [
             {"role": "system", "content": f"Bạn là một bot đang hỗ trợ các lập trình viên để trích xuất thông tin từ một trang web, đây là dữ liệu raw text được crawl về : {text} "},
             {"role": "user", "content": f"(Chỉ đưa ra thông tin được hỏi,không giải thích gì thêm, mỗi câu trả lời viết trên một dòng) : \n{mylist[2]}"},
@@ -113,33 +104,9 @@ class ExtractURL():
         )
         response = completion.choices[0].message
         row =row +response["content"].split("\n")
-
-        completion = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            temperature = 0.5,
-            max_tokens = 200,
-            messages = [
-            {"role": "system", "content": f"Bạn là một bot đang hỗ trợ các lập trình viên để trích xuất thông tin từ một trang web, đây là dữ liệu raw text được crawl về : {text} "},
-            {"role": "user", "content": f"(Chỉ đưa ra thông tin được hỏi, không giải thích gì thêm, mỗi câu trả lời viết trên một dòng) : \n{mylist[3]}"},
-        ]
-        ) 
-        response = completion.choices[0].message
-        row =row +response["content"].split("\n")
-        
-        completion = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo",
-            temperature = 0.5,
-            max_tokens = 300,
-            messages = [
-            {"role": "system", "content": f"Bạn là một bot đang hỗ trợ các lập trình viên để trích xuất thông tin từ một trang web, đây là dữ liệu raw text được crawl về : {text} "},
-            {"role": "user", "content": f"(Chỉ đưa ra thông tin được hỏi, không giải thích gì thêm, mỗi câu trả lời viết trên một dòng) : \n{mylist[4]}"},
-        ]
-        ) 
-        response = completion.choices[0].message
-        row =row +response["content"].split("\n")
-        row.append(soup.prettify())
-        row.append(text)
-        row.append(url)
+        row.append("html:"+soup.prettify())
+        row.append("text:"+text)
+        row.append("URL:"+url)
         fields = ["title","organization","deadline","type","benefits/value","educationLevel","majors","link","requirements","html_file","raw_text","url"]
         count=0
         scholar_dict ={}
@@ -147,7 +114,7 @@ class ExtractURL():
             if len(each)<=1:
                 continue
             info = each.split(':',maxsplit = 1)[1]
-            if (count>12) :
+            if (count>13) :
                 break
             if fields[count]=="type":
                 if "hỗ trợ khó khăn" in info.lower():
@@ -192,7 +159,7 @@ class ExtractURL():
                     majors = majors + "5,"
                 if "y tế" in info.lower():
                     majors = majors + "6,"
-                if "khoa học cơ bản" in info.lower() or "cơ bản" in info.lower():
+                if "khoa học tự nhiên" in info.lower() or "tự nhiên" in info.lower():
                     majors =majors +"7,"
                 if "sư phạm" in info.lower():
                     majors = majors + "8"
